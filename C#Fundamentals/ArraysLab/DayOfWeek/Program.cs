0aadc7c71247206707deb37783440e044efd1c89
beshkata <beshkata141@gmail.com>
2021-01-27 16:44:07 +0200

    Added ArraysLab.sln from 1 to 3 tasks

---
+++ C#Fundamentals/ArraysLab/DayOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main()
+        {
+            int dayNum = int.Parse(Console.ReadLine());
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (dayNum >= 1 && dayNum <= 7)
+            {
+                Console.WriteLine(days[dayNum - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/ArraysLab/RoundingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RoundingNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string strNumbers = Console.ReadLine();
+            decimal[] numbers = Array.ConvertAll(strNumbers.Split(" "), arrTemp => Convert.ToDecimal(arrTemp));
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i],MidpointRounding.AwayFromZero)}");
+            }
+
+        }
+    }
+}
