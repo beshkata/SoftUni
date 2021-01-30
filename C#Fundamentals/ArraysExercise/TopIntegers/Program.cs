960bbe348ab860fe5d9ce23f27d7f584d3c768fc
beshkata <beshkata141@gmail.com>
2021-01-30 11:00:36 +0200

    Added solutions for tasks 5 and 6

---
+++ C#Fundamentals/ArraysExercise/TopIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isTopInt = true;
+            string topInts = "";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i +1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isTopInt = false;
+                        break;
+                    }
+                }
+                if (isTopInt)
+                {
+                    topInts += numbers[i] + " ";
+                }
+                isTopInt = true;
+            }
+            Console.WriteLine(topInts);
+        }
+    }
+}
