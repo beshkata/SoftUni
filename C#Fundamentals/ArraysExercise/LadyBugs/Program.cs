7b9bcbd67e8950c07cfe9f0e85c608fa357aaa92
beshkata <beshkata141@gmail.com>
2021-02-03 10:18:53 +0200

    Added ArraysExercise.sln with all tasks solved

---
+++ C#Fundamentals/ArraysExercise/LadyBugs/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace LadyBugs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] initialBugIndexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            bool[] field = new bool[fieldSize];
+
+            foreach (var item in initialBugIndexes)
+            {
+                if (item >= 0 && item < field.Length)
+                {
+                    field[item] = true;
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] commands = command.Split();
+                int bugIndex = int.Parse(commands[0]);
+                string direction = commands[1];
+                int bugSpeed = int.Parse(commands[2]);
+
+                if (bugIndex < 0 || bugIndex > field.Length - 1 || field[bugIndex] == false)
+                {
+                    continue;
+                }
+
+                field[bugIndex] = false;
+
+                while (true)
+                {
+                    if (direction == "right")
+                    {
+                        bugIndex += bugSpeed;
+                    }
+                    else
+                    {
+                        bugIndex -= bugSpeed;
+                    }
+                    if (bugIndex < 0 || bugIndex > field.Length - 1)
+                    {
+                        break;
+                    }
+                    if (field[bugIndex] == false)
+                    {
+                        field[bugIndex] = true;
+                        break;
+                    }
+                }
+            }
+            foreach (var item in field)
+            {
+                if (item)
+                {
+                    Console.Write("1 ");
+                }
+                else
+                {
+                    Console.Write("0 ");
+                }
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/ArraysExercise/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int bestSequence = 0;
+            int numberBestSequence = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                int currentSequence = 1;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == currentNumber)
+                    {
+                        currentSequence++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+                if (currentSequence > bestSequence)
+                {
+                    bestSequence = currentSequence;
+                    numberBestSequence = currentNumber;
+                }
+            }
+
+            for (int i = 0; i < bestSequence; i++)
+            {
+                Console.Write($"{numberBestSequence} ");
+            }
+        }
+    }
+}
