8be4c5d9774519daec80dab4441ecc650213c335
beshkata <beshkata141@gmail.com>
2021-02-03 12:31:57 +0200

    Added ArraysMoreExercise.sln with four tasks solved

---
+++ C#Fundamentals/ArraysMoreExercise/EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int inputsCount = int.Parse(Console.ReadLine());
+            double[] inputsEncrypt = new double[inputsCount];
+
+            for (int i = 0; i < inputsCount; i++)
+            {
+                string input = Console.ReadLine();
+                double currentSum = 0.0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == 'A' || input[j] == 'a' || input[j] == 'E' || input[j] == 'e' ||
+                        input[j] == 'I' || input[j] == 'i' || input[j] == 'O' || input[j] == 'o' ||
+                        input[j] == 'U' || input[j] == 'u')
+                    {
+                        currentSum += input[j] * input.Length;
+                    }
+                    else
+                    {
+                        currentSum += input[j] / input.Length;
+                    }
+                }
+                inputsEncrypt[i] = currentSum;
+            }
+            Array.Sort(inputsEncrypt);
+
+            for (int i = 0; i < inputsEncrypt.Length; i++)
+            {
+                Console.WriteLine(inputsEncrypt[i]);
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/ArraysMoreExercise/RecursiveFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result = GetFibonacci(int.Parse(Console.ReadLine()));
+            Console.WriteLine(result);
+        }
+        static int GetFibonacci(int n)
+        {
+            
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                int fibonacciN = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+                return fibonacciN;
+            }
+        }
+    }
+}
