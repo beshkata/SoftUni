b91fea4dfe966742e64d9070d2d659636bb354b7
beshkata <beshkata141@gmail.com>
2021-02-20 12:43:28 +0200

    Added ListsExercise.sln with 1 and 2 tasks solved

---
+++ C#Fundamentals/ListsExercise/ChangeList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputs = input.Split();
+                string command = inputs[0];
+
+                if (command == "Delete")
+                {
+                    int element = int.Parse(inputs[1]);
+                    numbers.RemoveAll(x => x == element);
+                }
+                else
+                {
+                    int index = int.Parse(inputs[2]);
+                    int element = int.Parse(inputs[1]);
+
+                    numbers.Insert(index, element);
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ C#Fundamentals/ListsExercise/Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> train = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while(input != "end")
+            {
+                string[] inputs = input.Split().ToArray();
+                
+                if (inputs.Length == 2)
+                {
+                    int wagon = int.Parse(inputs[1]);
+                    train.Add(wagon);
+                }
+                else
+                {
+                    int passengers = int.Parse(inputs[0]);
+
+                    for (int i = 0; i < train.Count; i++)
+                    {
+                        if (train[i] + passengers <= capacity)
+                        {
+                            train[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', train));
+        }
+    }
+}
