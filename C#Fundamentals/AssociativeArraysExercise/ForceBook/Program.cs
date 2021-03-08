6164e6525a5fcbe3a515754a086e33ea9a0c0df5
beshkata <beshkata141@gmail.com>
2021-03-08 16:41:14 +0200

    Added AssociativeArraysExercise.sln tasks 9 and 10 solved Added AssociativeArraysMoreExercise.sln with tasks 1 and 2 solved

---
+++ C#Fundamentals/AssociativeArraysExercise/ForceBook/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForceBook
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string>> memberBySide = new Dictionary<string, List<string>>();
+            Dictionary<string, string> members = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if(line == "Lumpawaroo")
+                {
+                    break;
+                }
+
+                if (line.Contains(" | "))
+                {
+                    string[] parts = line.Split(" | ");
+
+                    string forceSide = parts[0];
+                    string forceUser = parts[1];
+
+                    if (members.ContainsKey(forceUser))
+                    {
+                        continue;
+                    }
+
+                    if (!memberBySide.ContainsKey(forceSide))
+                    {
+                        memberBySide.Add(forceSide, new List<string>());
+                    }
+
+                    memberBySide[forceSide].Add(forceUser);
+                    members.Add(forceUser, forceSide);
+                }
+                else
+                {
+                    string[] parts = line.Split(" -> ");
+                    string forceUser = parts[0];
+                    string forceSide = parts[1];
+
+                    if (!memberBySide.ContainsKey(forceSide))
+                    {
+                        memberBySide.Add(forceSide, new List<string>());
+                    }
+
+                    if (members.ContainsKey(forceUser))
+                    {
+                        string oldSide = members[forceUser];
+
+                        memberBySide[oldSide].Remove(forceUser);
+                        memberBySide[forceSide].Add(forceUser);
+                        members[forceUser] = forceSide;
+                    }
+                    else
+                    {
+                        memberBySide[forceSide].Add(forceUser);
+                        members.Add(forceUser, forceSide);
+                    }
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+
+            }
+            Dictionary<string, List<string>> result = memberBySide
+                .Where(x => x.Value.Count > 0)
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"Side: {kvp.Key}, Members: { kvp.Value.Count}");
+                kvp.Value.Sort();
+
+                foreach (var user in kvp.Value)
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+
+        }
+    }
+}
---
+++ C#Fundamentals/AssociativeArraysMoreExercise/Judge/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Judge
+{
+    class Program
+    {
+        static void Main()
+        {
+            //contest - user - points
+            Dictionary<string, Dictionary<string, int>> pointsByUserByContest = new Dictionary<string, Dictionary<string, int>>();
+            //user - contest - points
+            Dictionary<string, Dictionary<string, int>> pointsByContestByUser = new Dictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "no more time")
+                {
+                    break;
+                }
+                //{username} -> {contest} -> {points}
+                string[] parts = line.Split(" -> ");
+                string username = parts[0];
+                string contest = parts[1];
+                int points = int.Parse(parts[2]);
+
+                if (!pointsByUserByContest.ContainsKey(contest))
+                {
+                    pointsByUserByContest.Add(contest, new Dictionary<string, int>());
+                }
+
+                if (!pointsByContestByUser.ContainsKey(username))
+                {
+                    pointsByContestByUser.Add(username, new Dictionary<string, int>());
+                }
+
+                if (!pointsByUserByContest[contest].ContainsKey(username))
+                {
+                    pointsByUserByContest[contest].Add(username, 0);
+                }
+
+                if (!pointsByContestByUser[username].ContainsKey(contest))
+                {
+                    pointsByContestByUser[username].Add(contest, 0);
+                }
+
+                if (pointsByUserByContest[contest][username] < points)
+                {
+                    pointsByUserByContest[contest][username] = points;
+                }
+
+                if (pointsByContestByUser[username][contest] < points)
+                {
+                    pointsByContestByUser[username][contest] = points;
+                }
+            }
+
+            Dictionary<string, Dictionary<string, int>> orderedByContest = pointsByUserByContest
+                .ToDictionary
+                (
+                    x => x.Key,
+                    x => x.Value
+                        .OrderByDescending(p => p.Value)
+                        .ThenBy(n => n.Key)
+                        .ToDictionary(y => y.Key, y => y.Value)
+                );
+
+            foreach (var content in orderedByContest)
+            {
+                Console.WriteLine($"{content.Key}: {content.Value.Count} participants");
+                int count = 1;
+                foreach (var user in content.Value)
+                {
+                    Console.WriteLine($"{count}. {user.Key} <::> {user.Value}");
+                    count++;
+                }
+            }
+            Console.WriteLine("Individual standings:");
+
+            Dictionary<string, Dictionary<string, int>> orderedByUser = pointsByContestByUser
+                .OrderByDescending(x => x.Value.Values.Sum())
+                .ThenBy(n => n.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+            int counter = 1;
+            foreach (var user in orderedByUser)
+            {
+                Console.WriteLine($"{counter}. {user.Key} -> {user.Value.Values.Sum()}");
+                counter++;
+            }
+        }
+    }
+}
