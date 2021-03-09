782fd9b35513df9bd22a1672bc999e08bc6d899c
beshkata <beshkata141@gmail.com>
2021-03-09 16:38:15 +0200

    Added AssociativeArraysMoreExercise.sln with all tasks solved

---
+++ C#Fundamentals/AssociativeArraysMoreExercise/DragonArmy/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DragonArmy
+{
+    class Dragon
+    {
+        public Dragon(string type, string name, string damage, string health, string armor)
+        {
+            Type = type;
+            Name = name;
+            if (int.TryParse(damage, out int d))
+            {
+                Damage = d;
+            }
+            else
+            {
+                Damage = 45;
+            }
+
+            if (int.TryParse(health, out int h))
+            {
+                Health = h;
+            }
+            else
+            {
+                Health = 250;
+            }
+
+            if (int.TryParse(armor, out int a))
+            {
+                Armor = a;
+            }
+            else
+            {
+                Armor = 10;
+            }
+        }
+        public string Name { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Health { get; set; }
+
+        public int Armor { get; set; }
+
+        public string Type { get; set; }
+
+        public void UpdateDragon(string damage, string health, string armor)
+        {
+            if (int.TryParse(damage, out int d))
+            {
+                Damage = d;
+            }
+            else
+            {
+                Damage = 45;
+            }
+
+            if (int.TryParse(health, out int h))
+            {
+                Health = h;
+            }
+            else
+            {
+                Health = 250;
+            }
+
+            if (int.TryParse(armor, out int a))
+            {
+                Armor = a;
+            }
+            else
+            {
+                Armor = 10;
+            }
+        }
+
+        public override string ToString()
+        {
+            //-{Name} -> damage: {damage}, health: {health}, armor: {armor}
+            return $"-{Name} -> damage: {Damage}, health: {Health}, armor: {Armor}";
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<Dragon>> dragonByType = new Dictionary<string, List<Dragon>>();
+            List<Dragon> dragons = new List<Dragon>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                //{type} {name} {damage} {health} {armor}
+                string[] dragonInfo = Console.ReadLine().Split();
+
+                string type = dragonInfo[0];
+                string name = dragonInfo[1];
+                string damage = dragonInfo[2];
+                string health = dragonInfo[3];
+                string armor = dragonInfo[4];
+
+                int dragonIndex = GetDragonIndex(dragons, type, name);
+
+                if (dragonIndex < 0)
+                {
+                    Dragon dragon = new Dragon(type, name, damage, health, armor);
+                    dragons.Add(dragon);
+                }
+                else
+                {
+                    dragons[dragonIndex].UpdateDragon(damage, health, armor);
+                }
+            }
+
+            for (int i = 0; i < dragons.Count; i++)
+            {
+                if (!dragonByType.ContainsKey(dragons[i].Type))
+                {
+                    dragonByType.Add(dragons[i].Type, new List<Dragon> { dragons[i] });
+                }
+                else
+                {
+                    dragonByType[dragons[i].Type].Add(dragons[i]);
+                }
+            }
+
+            Dictionary<string, List<Dragon>> ordered = dragonByType
+                .ToDictionary(x => x.Key, x => x.Value.OrderBy(y => y.Name).ToList());
+
+            foreach (var kvp in ordered)
+            {
+                double avgDamage = 0.0;
+                double avgHealth = 0.0;
+                double avgArmor = 0.0;
+
+                int counter = 0;
+
+                foreach (var dragon in kvp.Value)
+                {
+                    avgDamage += dragon.Damage;
+                    avgHealth += dragon.Health;
+                    avgArmor += dragon.Armor;
+                    counter++;
+                }
+
+                avgDamage /= counter;
+                avgHealth /= counter;
+                avgArmor /= counter;
+
+                //{Type}::({damage}/{health}/{armor})
+                Console.WriteLine($"{kvp.Key}::({avgDamage:f2}/{avgHealth:f2}/{avgArmor:f2})");
+
+                foreach (var dragon in kvp.Value)
+                {
+                    Console.WriteLine(dragon);
+                }
+            }
+        }
+
+        private static int GetDragonIndex(List<Dragon> dragons, string type, string name)
+        {
+            int index = -1;
+
+            for (int i = 0; i < dragons.Count; i++)
+            {
+                if (dragons[i].Type == type && dragons[i].Name == name)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ C#Fundamentals/AssociativeArraysMoreExercise/Snowwhite/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Snowwhite
+{
+    class Dwarf
+    {
+        public Dwarf(string name, string color, int physics)
+        {
+            Name = name;
+            Color = color;
+            Physics = physics;
+        }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public int Physics { get; set; }
+
+        public int ColorCount { get; set; }
+
+        public override string ToString()
+        {
+            //({hatColor}) {name} <-> {physics}
+            return $"({Color}) {Name} <-> {Physics}";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dwarf> dwarves = new List<Dwarf>();
+            Dictionary<string, int> countByColor = new Dictionary<string, int>();
+
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Once upon a time")
+                {
+                    break;
+                }
+                //{dwarfName} <:> {dwarfHatColor} <:> {dwarfPhysics}
+                string[] dwarfInfo = line.Split(" <:> ");
+
+                string name = dwarfInfo[0];
+                string color = dwarfInfo[1];
+                int physics = int.Parse(dwarfInfo[2]);
+
+
+                int dwarfIndex = GetDwarfIndex(dwarves, name, color);
+
+
+                if (dwarfIndex < 0)
+                {
+                    Dwarf dwarf = new Dwarf(name, color, physics);
+                    dwarves.Add(dwarf);
+
+                    if (!countByColor.ContainsKey(color))
+                    {
+                        countByColor.Add(color, 0);
+                    }
+                    countByColor[color]++;
+                }
+                else
+                {
+                    if (dwarves[dwarfIndex].Physics < physics)
+                    {
+                        dwarves[dwarfIndex].Physics = physics;
+                    }
+                }
+            }
+
+            for (int i = 0; i < dwarves.Count; i++)
+            {
+                dwarves[i].ColorCount = countByColor[dwarves[i].Color];
+            }
+            List<Dwarf> result = dwarves
+                .OrderByDescending(d => d.Physics)
+                .ThenByDescending(c => c.ColorCount)
+                .ToList();
+
+            foreach (Dwarf dwarf1 in result)
+            {
+                Console.WriteLine(dwarf1);
+            }
+        }
+
+        private static int GetDwarfIndex(List<Dwarf> dwarves, string name, string color)
+        {
+            int index = -1;
+
+            for (int i = 0; i < dwarves.Count; i++)
+            {
+                if (dwarves[i].Name == name && dwarves[i].Color == color)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+
+            return index;
+        }
+    }
+}
