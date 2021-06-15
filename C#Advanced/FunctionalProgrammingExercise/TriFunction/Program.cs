64ec45f388bdba953ae60da7ccb844607c3f6fbd
beshkata <beshkata141@gmail.com>
2021-06-15 08:48:14 +0300

    Added FunctionalProgrammingExercise.sln with task 12 solved

---
+++ C#Advanced/FunctionalProgrammingExercise/TriFunction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Func<string, int, bool> isBiggerOrEqual = (a, b) => NameToInt(a,b) >= b;
+
+            foreach (var name in names)
+            {
+                if (isBiggerOrEqual(name, num))
+                {
+                    Console.WriteLine(name);
+                    return;
+                }
+            }
+        }
+
+        static int NameToInt (string name, int number)
+        {
+            int nameToInt = 0;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                nameToInt += name[i];
+            }
+            return nameToInt;
+        }
+    }
+}
