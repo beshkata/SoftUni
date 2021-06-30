4f7f65e2f5ad2e78259050f8e1791ab011fcaacb
beshkata <beshkata141@gmail.com>
2021-06-30 14:01:39 +0300

    Added InheritanceLab.sln

---
+++ C#OOP/InheritanceLab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            foreach (string str in collection)
+            {
+                stack.Push(str);
+            }
+
+            return stack;
+        }
+    }
+}
