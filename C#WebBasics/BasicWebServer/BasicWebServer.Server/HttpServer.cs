2a5dab49f2e41689e444c8b6521d553afa797f9a
beshkata <Preslava0924Elena>
2022-01-21 16:19:20 +0200

    Added C#WebBasics

---
+++ C#WebBasics/BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress, port);
+        }
+
+        public void Start()
+        {
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+
+                WriteResponse(networkStream, "Hello from the server");
+
+                connection.Close();
+            }
+
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string content)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{content}";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            networkStream.Write(responseBytes);
+
+        }
+    }
+}
