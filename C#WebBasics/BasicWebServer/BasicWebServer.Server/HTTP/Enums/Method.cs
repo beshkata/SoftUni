1b38af280cd2743e2598187cb6c3c984144eaaae
beshkata <Preslava0924Elena>
2022-01-26 16:39:51 +0200

    Added Http folder with Header.cs, HeaderCollection.cs, Request.cs and Response.cs classes

---
+++ C#WebBasics/BasicWebServer/BasicWebServer.Server/HTTP/Enums/Method.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicWebServer.Server.HTTP.Enums
+{
+    public enum Method
+    {
+        Get = 1,
+        Post = 2,
+        Put = 3,
+        Delete = 4
+    }
+}
---
+++ C#WebBasics/BasicWebServer/BasicWebServer.Server/HTTP/Request.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BasicWebServer.Server.HTTP.Enums;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Request
+    {
+        public Method Method { get; private set; }
+
+        public string Url { get; private set; }
+
+        public HeaderCollection Headers { get; private set; }
+
+        public string Body { get; private set; }
+
+        public static Request Parse(string request)
+        {
+            var lines = request.Split(Environment.NewLine);//"\r\n"
+            var startLine = lines.First().Split(" ");
+            var method = ParseMethod(startLine[0]);
+            var url = startLine[1];
+            var headers = ParseHeaders(lines.Skip(1));
+            var bodyLines = lines.Skip(headers.Count + 2);
+            var body = string.Join(Environment.NewLine,)
+        }
+
+        private static HeaderCollection ParseHeaders(IEnumerable<string> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Method ParseMethod(string method)
+        {
+            try
+            {
+                return Enum.Parse<Method>(method, true);
+            }
+            catch (Exception)
+            {
+                throw new InvalidOperationException($"Method {method} is not supported");
+            }
+        }
+    }
+}
