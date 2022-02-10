4a38c3ccdef795b86d4afd91af43930ce4c18028
beshkata <Preslava0924Elena>
2022-02-10 15:14:46 +0200

    3. Introduce Models Step 1: Modify ViewResponse Class

--- C#WebBasics/BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
+++ C#WebBasics/BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
@@ -1,12 +1,13 @@
 ﻿using BasicWebServer.Server.HTTP;
 using System.IO;
+using System.Linq;
 
 namespace BasicWebServer.Server.Responses
 {
     public class ViewResponse : ContentResponse
     {
         private const char PathSeparator = '/';
-        public ViewResponse(string viewName, string controllerName)
+        public ViewResponse(string viewName, string controllerName, object model = null)
             : base("", ContentType.Html)
         {
             if (!viewName.Contains(PathSeparator))
@@ -19,7 +20,35 @@ namespace BasicWebServer.Server.Responses
                 + ".cshtml");
             var viewContent = File.ReadAllText(viewPath);
 
+            if (model != null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
             Body = viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(pr => new
+                {
+                    pr.Name,
+                    Value = pr.GetValue(model)
+                });
+
+            foreach (var entry in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace(
+                    $"{openingBrackets}{entry.Name}{closingBrackets}",
+                    entry.Value.ToString());
+            }
+
+            return viewContent;
         }
     }
 }
