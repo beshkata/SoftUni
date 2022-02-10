87fb4c3effc92a94c972d075d4516a7e729f4283
beshkata <Preslava0924Elena>
2022-02-10 13:08:27 +0200

    Modify Controller to Return View

--- C#WebBasics/BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
+++ C#WebBasics/BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -1,5 +1,7 @@
 ﻿using BasicWebServer.Server.HTTP;
 using BasicWebServer.Server.Responses;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace BasicWebServer.Server.Controllers
 {
@@ -32,5 +34,13 @@ namespace BasicWebServer.Server.Controllers
         protected Response NotFound() => new NotFoundResponse();
         protected Response Redirect(string location) => new RedirectResponse(location);
         protected Response File(string fileName) => new FileResponse(fileName);
+
+        protected Response View([CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName());
+
+        private string GetControllerName()
+            => GetType().Name
+            .Replace(nameof(Controller), string.Empty);
+        
     }
 }
