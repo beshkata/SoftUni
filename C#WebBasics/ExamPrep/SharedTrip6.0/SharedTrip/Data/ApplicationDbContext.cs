6552a9c562c1bf425de80fccab3c516dcd5bb672
beshkata <beshkata141@gmail.com>
2022-02-12 11:44:25 +0200

    Added DbSets and composite key in ApplicationDbContext class

--- C#WebBasics/ExamPrep/SharedTrip6.0/SharedTrip/Data/ApplicationDbContext.cs
+++ C#WebBasics/ExamPrep/SharedTrip6.0/SharedTrip/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharedTrip.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using SharedTrip.Data.Models;
 
     public class ApplicationDbContext : DbContext
     {
@@ -18,7 +19,14 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserTrip>()
+                .HasKey(ut => new { ut.UserId, ut.TripId });
         }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
     }
 }
