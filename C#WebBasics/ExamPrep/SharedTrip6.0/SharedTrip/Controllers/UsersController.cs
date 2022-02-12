a08b889e852c1393b8af8a33e2eca93dc62b91c2
beshkata <beshkata141@gmail.com>
2022-02-12 20:52:40 +0200

    Added UserService in Startup.cs.

---
+++ C#WebBasics/ExamPrep/SharedTrip6.0/SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Controllers
+{
+    public class UsersController : Controller
+    {
+        public UsersController(Request request)
+            : base(request)
+        {
+        }
+
+        public Response Login()
+        {
+            return View();
+        }
+
+        public Response Register()
+        {
+            return View();
+        }
+    }
+}
