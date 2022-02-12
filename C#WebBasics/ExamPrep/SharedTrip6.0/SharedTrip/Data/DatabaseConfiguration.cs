d7adceae0c90a4d095d6529ef0b15fd8ce84047b
beshkata <beshkata141@gmail.com>
2022-02-12 11:06:59 +0200

    Connection string changed in DatabaseConfiguration file

--- C#WebBasics/ExamPrep/SharedTrip6.0/SharedTrip/Data/DatabaseConfiguration.cs
+++ C#WebBasics/ExamPrep/SharedTrip6.0/SharedTrip/Data/DatabaseConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseConfiguration
     {
         public const string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=SharedTrip;Trusted_Connection=True;Integrated Security=True;";
+            @"Server=BESHO-PC\SQLEXPRESS;Database=SharedTrip;Trusted_Connection=True;Integrated Security=True;";
     }
 }
