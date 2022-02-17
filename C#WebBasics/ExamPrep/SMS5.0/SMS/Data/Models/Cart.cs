242e5d179e466a25d46486fa93aecb33fbd5d77a
beshkata <beshkata141@gmail.com>
2022-02-17 11:46:13 +0200

    Database models and database initialMigration created

---
+++ C#WebBasics/ExamPrep/SMS5.0/SMS/Data/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using SMS.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMS.Data.Models
+{
+    public class Cart
+    {
+        [Key]
+        [MaxLength(GlobalConstants.GuidMaxLength)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        //[Required]
+        //[MaxLength(GlobalConstants.GuidMaxLength)]
+        //[ForeignKey(nameof(User))]
+        //public string UserId { get; set; }
+
+        //public User User { get; set; }
+
+        //public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
---
+++ C#WebBasics/ExamPrep/SMS5.0/SMS/Data/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using SMS.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMS.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        [MaxLength(GlobalConstants.GuidMaxLength)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MaxLength(GlobalConstants.ProductNameMaxLength)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        [ForeignKey(nameof(Cart))]
+        public string CartId { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+ }
